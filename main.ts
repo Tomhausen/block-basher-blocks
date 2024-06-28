c8450f567628e8a0bde648947cc3b41b195d2b98
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2024-06-28 14:41:28 +0100

    w1 content

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 namespace SpriteKind {
-    export const cursor = SpriteKind.create()
-}
-function aim () {
-    angle = spriteutils.radiansToDegrees(spriteutils.angleFrom(ghost, pointer))
-    transformSprites.rotateSprite(aim_sprite, angle)
+    export const Cursor = SpriteKind.create()
+    export const Effect = SpriteKind.create()
+    export const Path = SpriteKind.create()
 }
 function make_lives_text (col: number, row: number) {
     location = tiles.getTileLocation(col, row)
@@ -16,6 +14,8 @@ function spawn_row () {
     for (let col = 0; col <= 9; col++) {
         if (randint(1, 5) > 1) {
             spawn_block(col, 0)
+        } else if (randint(1, 5) == 1) {
+            spawn_bonus_ball(col, 0)
         }
     }
     music.knock.play()
@@ -29,7 +29,14 @@ function move_row () {
         }
         move_block(location.column, location.row)
     }
+    for (let value of sprites.allOfKind(SpriteKind.Food)) {
+        value.y += 16
+    }
 }
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Food, function (sprite, otherSprite) {
+    proj_count += 1
+    sprites.destroy(otherSprite)
+})
 function fire () {
     fire_angle = spriteutils.angleFrom(ghost, pointer)
     for (let index = 0; index < proj_count; index++) {
@@ -37,6 +44,9 @@ function fire () {
         proj.setPosition(ghost.x, ghost.y)
         proj.setBounceOnWall(true)
         spriteutils.setVelocityAtAngle(proj, fire_angle, proj_speed)
+        if (one_shot_active) {
+            proj.startEffect(effects.fire)
+        }
         pause(50)
     }
 }
@@ -52,9 +62,19 @@ scene.onHitWall(SpriteKind.Projectile, function (proj, location) {
     }
 })
 browserEvents.MouseLeft.onEvent(browserEvents.MouseButtonEvent.Pressed, function (x, y) {
-    aim_sprite.setFlag(SpriteFlag.Invisible, false)
-    aim_sprite.x = ghost.x
-    aim_sprite.y = ghost.y
+    draw_path = true
+})
+function make_power_up_bar () {
+    one_shot_active = false
+    power_up_bar = statusbars.create(160, 2, StatusBarKind.Magic)
+    power_up_bar.bottom = 120
+    power_up_bar.value = 0
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (power_up_bar.value >= power_up_bar.max) {
+        one_shot_active = true
+        power_up_bar.value = 0
+    }
 })
 function cycle_blocks () {
     move_row()
@@ -64,6 +84,7 @@ function setup_vars () {
     proj_count = 10
     proj_speed = 200
     lives = 3
+    draw_path = false
 }
 function spawn_block (col: number, row: number) {
     location = tiles.getTileLocation(col, row)
@@ -72,6 +93,30 @@ function spawn_block (col: number, row: number) {
     block_life = lives + randint(-2, 2)
     tiles.setDataNumber(location, "life", block_life)
     make_lives_text(location.column, location.row)
+}
+function path () {
+    aim_sprite.setPosition(ghost.x, ghost.y)
+    direction = spriteutils.angleFrom(ghost, pointer)
+    x_vector = Math.cos(direction)
+    y_vector = Math.sin(direction)
+    dot = image.create(2, 2)
+    dot.fill(1)
+    for (let index = 0; index < 10; index++) {
+        dot_sprite = sprites.create(dot, SpriteKind.Path)
+        dot_sprite.setPosition(aim_sprite.x, aim_sprite.y)
+        for (let index = 0; index < 15; index++) {
+            aim_sprite.x += x_vector
+            if (tiles.tileAtLocationIsWall(aim_sprite.tilemapLocation())) {
+                x_vector = x_vector * -1
+                aim_sprite.x += x_vector
+            }
+            aim_sprite.y += y_vector
+            if (tiles.tileAtLocationIsWall(aim_sprite.tilemapLocation())) {
+                y_vector = y_vector * -1
+                aim_sprite.y += y_vector
+            }
+        }
+    }
 }
 function move_block (col: number, row: number) {
     location = tiles.getTileLocation(col, row)
@@ -101,7 +146,7 @@ function move_block (col: number, row: number) {
     tiles.moveData(location, new_location, true)
 }
 function setup () {
-    music.setVolume(20)
+    music.setVolume(60)
     tiles.setCurrentTilemap(tilemap`level`)
     for (let index = 0; index < 3; index++) {
         cycle_blocks()
@@ -111,7 +156,7 @@ function block_damage (col: number, row: number) {
     location = tiles.getTileLocation(col, row)
     new_life = tiles.readDataNumber(location, "life") - 1
     sprites.destroy(tiles.readDataSprite(location, "text"))
-    if (new_life < 1) {
+    if (new_life < 1 || one_shot_active) {
         tiles.setTileAt(location, img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -132,27 +177,37 @@ function block_damage (col: number, row: number) {
             `)
         tiles.setWallAt(location, false)
         info.changeScoreBy(10)
+        scene.cameraShake(4, 500)
+        music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.UntilDone)
     } else {
         tiles.setDataNumber(location, "life", new_life)
         make_lives_text(location.column, location.row)
+        music.play(music.melodyPlayable(music.footstep), music.PlaybackMode.UntilDone)
+        effect_sprite = sprites.create(image.create(16, 16), SpriteKind.Effect)
+        effect_sprite.image.fill(2)
+        tiles.placeOnTile(effect_sprite, location)
+        effect_sprite.setFlag(SpriteFlag.Invisible, true)
+        effect_sprite.startEffect(effects.ashes, 200)
+        power_up_bar.value += 1
     }
 }
 function make_sprites () {
     ghost = sprites.create(assets.image`ghost`, SpriteKind.Player)
     ghost.bottom = 120
     ghost.setStayInScreen(true)
-    pointer = sprites.create(image.create(1, 1), SpriteKind.cursor)
+    pointer = sprites.create(image.create(1, 1), SpriteKind.Cursor)
     pointer.image.fill(1)
     pointer.setFlag(SpriteFlag.Invisible, true)
 }
 function setup_aim_sprite () {
-    aim_sprite = sprites.create(image.create(150, 150), 0)
-    aim_sprite.image.fillRect(74, 74, 75, 2, 2)
+    aim_sprite = sprites.create(image.create(2, 2), 0)
+    aim_sprite.image.fill(1)
+    aim_sprite.setFlag(SpriteFlag.GhostThroughWalls, true)
     aim_sprite.z = -1
     aim_sprite.setFlag(SpriteFlag.Invisible, true)
 }
 browserEvents.MouseLeft.onEvent(browserEvents.MouseButtonEvent.Released, function (x, y) {
-    aim_sprite.setFlag(SpriteFlag.Invisible, true)
+    draw_path = false
     if (sprites.allOfKind(SpriteKind.Projectile).length < 1) {
         timer.background(function () {
             fire()
@@ -161,7 +216,7 @@ browserEvents.MouseLeft.onEvent(browserEvents.MouseButtonEvent.Released, functio
         ghost,
         assets.animation`ghost throw`,
         100,
-        false
+        true
         )
     }
 })
@@ -170,34 +225,56 @@ sprites.onDestroyed(SpriteKind.Projectile, function (sprite) {
         game.over(true)
     }
     if (sprites.allOfKind(SpriteKind.Projectile).length < 1) {
+        one_shot_active = false
         cycle_blocks()
     }
 })
+function spawn_bonus_ball (col: number, row: number) {
+    location = tiles.getTileLocation(col, row)
+    bonus_ball = sprites.create(assets.image`bonus`, SpriteKind.Food)
+    tiles.placeOnTile(bonus_ball, location)
+    bonus_ball.setFlag(SpriteFlag.AutoDestroy, true)
+}
+let bonus_ball: Sprite = null
+let effect_sprite: Sprite = null
 let new_life = 0
 let block_image: Image = null
 let new_location: tiles.Location = null
+let dot_sprite: Sprite = null
+let dot: Image = null
+let y_vector = 0
+let x_vector = 0
+let direction = 0
+let aim_sprite: Sprite = null
 let lives = 0
+let power_up_bar: StatusBarSprite = null
+let draw_path = false
+let one_shot_active = false
 let proj_speed = 0
 let proj: Sprite = null
-let proj_count = 0
+let pointer: Sprite = null
 let fire_angle = 0
+let proj_count = 0
+let ghost: Sprite = null
 let all_blocks: tiles.Location[] = []
 let lives_text: TextSprite = null
 let block_life = 0
 let location: tiles.Location = null
-let aim_sprite: Sprite = null
-let pointer: Sprite = null
-let ghost: Sprite = null
-let angle = 0
 setup_vars()
 make_sprites()
 setup_aim_sprite()
 setup()
+make_power_up_bar()
 game.onUpdate(function () {
     pointer.x = browserEvents.getMouseCameraX()
     pointer.y = browserEvents.getMouseCameraY()
     if (pointer.y >= ghost.y - 5) {
         pointer.y = ghost.y - 5
     }
-    aim()
+    if (sprites.allOfKind(SpriteKind.Path).length > 0) {
+        sprites.destroyAllSpritesOfKind(SpriteKind.Path)
+    }
+    if (draw_path) {
+        path()
+    }
 })
